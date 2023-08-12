d81cbcbf8670fd051a878002dd9ad107694cf60c
Nashat Samy <nashat.masamy@gmail.com>
2023-08-12 09:50:48 +0300

    project setup

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    styles: ['./src/css-reset.css', './src/style.css'],
+    mainPage: ['./src/main.js'],
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'BattleShip',
+      template: './src/template.html',
+    }),
+    new FaviconsWebpackPlugin('./src/icons/favicon.png'),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
